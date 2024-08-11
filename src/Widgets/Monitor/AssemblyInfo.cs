667e15fa30fb40d87ddbede9c31424bd4ee8fe22
creewick <ilia_y@mail.ru>
2024-08-11 23:15:33 +0300

    feat(Monitor): Single Metric widget

---
+++ src/Widgets/Monitor/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using Monitor.Locales;
+using Monitor.Models;
+using Monitor.Views;
+using Monitor.Views.Settings;
+using uWidgets.Core.Models.Attributes;
+
+[assembly: AssemblyCompany("creewick")]
+[assembly: AssemblyVersion("1.0.0")]
+
+[assembly: WidgetInfo(typeof(SingleMetric), typeof(SingleMetricModel), typeof(SingleMetricSettings), "Monitor_SingleMetric_Title", "Monitor_SingleMetric_Subtitle")]
+[assembly: Locale(typeof(Locale), "Monitor")]
