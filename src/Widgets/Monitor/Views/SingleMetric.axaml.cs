dd78ba2fad59899e05aa4e181ab7e14baf752943
creewick <ilia_y@mail.ru>
2024-08-28 08:48:21 +0300

    feat(Monitor): Single Metric smaller view

--- src/Widgets/Monitor/Views/SingleMetric.axaml.cs
+++ src/Widgets/Monitor/Views/SingleMetric.axaml.cs
@@ -1,4 +1,5 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Monitor.Models;
 using Monitor.ViewModels;
 
@@ -12,6 +13,25 @@ public partial class SingleMetric : UserControl
     public SingleMetric(SingleMetricModel model)
     {
         DataContext = new SingleMetricViewModel(model);
+        SizeChanged += OnSizeChanged;
+        Unloaded += OnUnloaded;
         InitializeComponent();
+    }
+
+    private void OnUnloaded(object? sender, RoutedEventArgs e)
+    {
+        SizeChanged -= OnSizeChanged;
+        Unloaded -= OnUnloaded;
+    }
+
+    private void OnSizeChanged(object? sender, SizeChangedEventArgs e)
+    {
+        const int smallSize = 100;
+        var small = e.NewSize is { Height: < smallSize };
+        
+        Grid.RowDefinitions = RowDefinitions.Parse(small ? "*" : "*,*");
+        Grid.SetRow(Text, small ? 0 : 1);
+        Text.IsVisible = !small;
+        Margin = new(small ? 6 : 12);
     }
 }
